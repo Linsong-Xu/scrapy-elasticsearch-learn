05ff40156b72f03ce042ba45a44eaef32f88ea14
Lin-song Xu <34213047+Linsong-Xu@users.noreply.github.com>
2019-03-02 20:43:28 +0800

    Update settings.py

--- ArticleSpider/ArticleSpider/settings.py
+++ ArticleSpider/ArticleSpider/settings.py
@@ -117,6 +117,6 @@ RANDOM_UA_TYPE = "random"
 MYSQL_HOST = '127.0.0.1'
 MYSQL_DBNAME = 'article_spider'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'xulinsong56I'
+MYSQL_PASSWORD = 'root'
 SQL_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 SQL_DATE_FORMAT = '%Y-%m-%d'
